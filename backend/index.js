1d615efe97be14a98819edd9385e4bbae84636df
Abhishek Verma <114756516+abhishekmill@users.noreply.github.com>
2024-10-08 15:35:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,18 +15,15 @@ const allowedOrigins = [
   "http://192.168.1.11:5173",
 ];
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true); // Allow the origin
-      } else {
-        callback(new Error("Not allowed by CORS")); // Block the origin
-      }
-    },
-    credentials: true,
-  })
-);
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 dotenv.config();
 const port = 4000;
